99b011b8e2a71fb059570269cd22c399d242c16c
Jaegoomon <jaegoo1199@google.com>
2021-06-07 00:55:56 +0900

    refactoring issue trend analysis

--- issue_trend_analysis.py
+++ issue_trend_analysis.py
@@ -1,11 +1,12 @@
 
 from env.env import ARTICLE_2015, ARTICLE_2016, ARTICLE_2017, FIRST_CLUSTERING_NUMBER, FIRST_RECLUSTERING_2015_SIM, FIRST_RECLUSTERING_2016_SIM, FIRST_RECLUSTERING_2017_SIM, RECLUSTERING_2015_PATH, RECLUSTERING_2016_PATH, RECLUSTERING_2017_PATH
-from utils.utils import bfs
+from utils.utils import bfs, capital
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 import numpy as np
 from nltk.corpus import stopwords
-from src.keyword_extraction import KeywordExtractor
+import pandas as pd
+from src.keyword_extractor import KeywordExtractor
 
 YEAR = [2015, 2016, 2017]
 ARTICLE = [ARTICLE_2015, ARTICLE_2016, ARTICLE_2017]
@@ -17,14 +18,21 @@ SAVE_PATH = [RECLUSTERING_2015_PATH,
 
 def main():
     for year in range(3):
+        # extract raw data
+        article = pd.read_csv(ARTICLE[year])
         keywordExtractor = KeywordExtractor()
-        extracted_keyword = keywordExtractor.extract(ARTICLE[year])
+
+        extracted_keyword = []
+        for cluster_idx in range(len(set(article.cluster))):
+            cluster = article[article['cluster'] == cluster_idx]
+            # extract corresponding cluster
+            keyword_list = keywordExtractor.extract(cluster)[:30]
+            extracted_keyword.append(" ".join(keyword_list))
 
         # vectorize using keyword n-grams
         vectorizer = CountVectorizer(max_features=1500, ngram_range=(
             2, 5), min_df=1, max_df=0.7, stop_words=stopwords.words('english'))
         X_count = vectorizer.fit_transform(extracted_keyword)
-
         # apply tfidf
         # X_tfidf = TfidfTransformer().fit_transform(X_count).toarray()
 
@@ -36,30 +44,34 @@ def main():
         index_row, index_col = indices
         similarity_result = zip(index_row, index_col)
         similarity_set = set()
+
+        # join similar clusters
         for (r, c) in similarity_result:
             if (r != c):
                 p = (r, c) if r < c else (c, r)
                 similarity_set.add(p)
-
         similarity_result = list(similarity_set)
 
         # create sub cluster using bfs algorithm
         cluster_index = [i for i in range(FIRST_CLUSTERING_NUMBER)]
         cluster = []
-
         for i in cluster_index:
             if i in [e for sub in cluster for e in sub]:
                 continue
-
             sub_cluster = []
             sub_cluster.append(i)
             cluster.append(bfs(sub_cluster, 0, similarity_result))
 
-        # reclustering
-        top_ranking = keywordExtractor.recluster(ARTICLE[year], cluster)
+        # reclustering by similarity
+        top_ranking = dict()
+        for sub_cluster in cluster:
+            sub_cluster = article[article['cluster'].isin(sub_cluster)]
+            top_issue = keywordExtractor.extract(sub_cluster)
+            top_ranking[capital(top_issue[0])] = sub_cluster.shape[0]
 
         top_ranking = sorted(top_ranking.items(), reverse=True,
                              key=lambda x: x[1])[:10]
+
         print("=======================")
         print("\t", YEAR[year])
         print("=======================")
@@ -67,7 +79,7 @@ def main():
             print(topic, end=' ')
         print()
 
-        keywordExtractor.extract_file(ARTICLE[year], SAVE_PATH[year], cluster)
+        keywordExtractor.extract_file(article, SAVE_PATH[year], cluster)
 
 
 if __name__ == "__main__":
---
+++ src/keyword_extractor.py
@@ -0,0 +1,39 @@
+import ast
+from utils.utils import capital, preprocessing
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.corpus import stopwords
+
+
+class KeywordExtractor:
+    def __init__(self) -> None:
+        pass
+
+    def extract(self, cluster: pd.DataFrame) -> list:
+        keyword_set = cluster.keyword
+
+        ne_set = cluster['ne']
+        ne_set = [ne.lower()
+                  for ne_list in ne_set for ne in ast.literal_eval(ne_list)]
+
+        doc_num = len(keyword_set)
+        keyword_set = [preprocessing(keyword.replace(
+            ' ,', ' '), list(ne_set)) for keyword in keyword_set]
+
+        # vectorize keywords
+        max_df = doc_num * 0.7 if doc_num > 1 else 1
+        vectorizer = CountVectorizer(max_features=1500, ngram_range=(
+            2, 5), min_df=1, max_df=max_df, stop_words=stopwords.words('english'))
+        X_count = vectorizer.fit_transform(keyword_set).toarray()
+
+        # extract top nth keywords
+        result = pd.DataFrame(
+            X_count, columns=vectorizer.get_feature_names())
+        result = list(result.sum(axis=0).sort_values(
+            ascending=False).keys())
+        return result
+
+    def extract_file(self, article: pd.DataFrame, save_path: str, cluster: list) -> None:
+        for i in cluster:
+            article.loc[article['cluster'].isin(i), 'cluster'] = i[0]
+            article.to_csv(save_path)
