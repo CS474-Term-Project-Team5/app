d648e3d1a6708faa78cd1b87af9e16bfbf7705b3
Jaegoomon <jaegoo1199@google.com>
2021-06-06 14:07:13 +0900

    first commit

---
+++ issue_trend_analysis/env/env.py
@@ -0,0 +1,17 @@
+FIRST_CLUSTERING_NUMBER = 30
+SECOND_CLUSTERING_NUMBER = 20
+
+
+ARTICLE_2015 = r'/root/issue_trend_analysis/data/koreaherald_2015_30_ver.csv'
+ARTICLE_2016 = r'/root/issue_trend_analysis/data/koreaherald_2016_30_ver.csv'
+ARTICLE_2017 = r'/root/issue_trend_analysis/data/koreaherald_2017_30_ver.csv'
+
+
+FIRST_RECLUSTERING_2015_SIM = 0.055
+FIRST_RECLUSTERING_2016_SIM = 0.125
+FIRST_RECLUSTERING_2017_SIM = 0.2
+
+
+RECLUSTERING_2015_PATH = r'/root/issue_trend_analysis/data/recluster/koreaherald_2015_recluster_ver.csv'
+RECLUSTERING_2016_PATH = r'/root/issue_trend_analysis/data/recluster/koreaherald_2016_recluster_ver.csv'
+RECLUSTERING_2017_PATH = r'/root/issue_trend_analysis/data/recluster/koreaherald_2017_recluster_ver.csv'
